1d1b8368f395ee6569ddb26c67251f8038d4c393
Firepup650 <70233190+Firepup6500@users.noreply.github.com>
2023-08-12 23:51:23 -0500

    Indents are weird

--- assets/javascripts/discourse/pre-initializers/extend-user-for-my-plugin.js
+++ assets/javascripts/discourse/pre-initializers/extend-user-for-my-plugin.js
@@ -5,19 +5,18 @@ import { withPluginApi } from "discourse/lib/plugin-api";
 export default {
   name: "extend-user-for-my-plugin",
   before: "inject-discourse-objects",
-
+  
   initializeWithApi(api){
     api.modifyClass('model:user', {
       pluginId: 'customProfileLink',
       customProfileLink: Ember.computed('user_fields.@each.value', function() {
         const fieldName = settings.custom_profile_link_user_field;
-        
         const siteUserFields = Site.currentProp('user_fields');
         
         if (Ember.isEmpty(siteUserFields)) return null;
         
-        const field = siteUserFields.filterBy('name', fieldName)[0]
-          if (!field) return null;
+        const field = siteUserFields.filterBy('name', fieldName)[0];
+        if (!field) return null;
         const fieldId = field.get('id');
         const userFields = this.get('user_fields');
         if (!userFields || !userFields[fieldId]) return null;
@@ -25,7 +24,7 @@ export default {
         return userFields[fieldId];
       })
     }
-                    },
+  },
   initialize() {
     withPluginApi("0.12.1", this.initializeWithApi);
   },
