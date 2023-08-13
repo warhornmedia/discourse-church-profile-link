4a3a0541a1a94291f370c5acb538eec0aa2f3500
Firepup650 <70233190+Firepup6500@users.noreply.github.com>
2023-08-12 23:42:13 -0500

    Whoops

--- assets/javascripts/discourse/pre-initializers/extend-user-for-my-plugin.js
+++ assets/javascripts/discourse/pre-initializers/extend-user-for-my-plugin.js
@@ -10,22 +10,22 @@ export default {
     api.modifyClass('model:user', {
       pluginId: 'customProfileLink',
       customProfileLink: Ember.computed('user_fields.@each.value', function() {
-          const fieldName = settings.custom_profile_link_user_field;
-          
-          const siteUserFields = Site.currentProp('user_fields');
-          if (Ember.isEmpty(siteUserFields)) return null;
-          
-          const field = siteUserFields.filterBy('name', fieldName)[0]
+        const fieldName = settings.custom_profile_link_user_field;
+        
+        const siteUserFields = Site.currentProp('user_fields');
+        
+        if (Ember.isEmpty(siteUserFields)) return null;
+        
+        const field = siteUserFields.filterBy('name', fieldName)[0]
           if (!field) return null;
-          
-          const fieldId = field.get('id');
-          const userFields = this.get('user_fields');
-          if (!userFields || !userFields[fieldId]) return null;
-          
-          return userFields[fieldId];
+        const fieldId = field.get('id');
+        const userFields = this.get('user_fields');
+        if (!userFields || !userFields[fieldId]) return null;
+        
+        return userFields[fieldId];
       })
-  },
-
+    }
+                    },
   initialize() {
     withPluginApi("0.12.1", this.initializeWithApi);
   },
