406a494c3f2e48be241b49bccc9a257ce590bcee
Firepup650 <70233190+Firepup6500@users.noreply.github.com>
2023-08-13 11:33:06 -0500

    Trying everything at this point

--- assets/javascripts/discourse/pre-initializers/extend-user-for-my-plugin.js
+++ assets/javascripts/discourse/pre-initializers/extend-user-for-my-plugin.js
@@ -8,28 +8,23 @@ export default {
   
   initialize() {
     console.log("[DEBUG] Init CPL");
-    withPluginApi("0.12.1", this.initializeWithApi);
-  },
-  
-  initializeWithApi(api){
-    api.modifyClass('model:user', {
-      pluginId: 'customProfileLink',
-      customProfileLink: Ember.computed('user_fields.@each.value', function() {
-        console.log("[DEBUG] Modifiying");
-        const fieldName = settings.custom_profile_link_user_field;
-        const siteUserFields = Site.currentProp('user_fields');
-        
-        if (Ember.isEmpty(siteUserFields)) {return null;}
-        
-        const field = siteUserFields.filterBy('name', fieldName)[0];
-        if (!field) {return null;}
-        const fieldId = field.get('id');
-        const userFields = this.get('user_fields');
-        if (!userFields || !userFields[fieldId]) {return null;}
-        
-        console.log("[DEBUG] Finished");
-        return userFields[fieldId];
-        })
-      });
+    withPluginApi("0.12.1", (api) => {
+      api.modifyClass('model:user', {
+        pluginId: 'customProfileLink',
+        customProfileLink: Ember.computed('user_fields.@each.value', function() {
+          console.log("[DEBUG] Modifiying");
+          const fieldName = settings.custom_profile_link_user_field;
+          const siteUserFields = Site.currentProp('user_fields');
+          if (Ember.isEmpty(siteUserFields)) {return null;}
+          const field = siteUserFields.filterBy('name', fieldName)[0];
+          if (!field) {return null;}
+          const fieldId = field.get('id');
+          const userFields = this.get('user_fields');
+          if (!userFields || !userFields[fieldId]) {return null;}
+          console.log("[DEBUG] Finished");
+          return userFields[fieldId];
+          })
+        });
+      })
     }
-}
+  }
